7337fcccbc83d459e46390a730d75cb256dc5449
MHeinC <98407467+MHeinC@users.noreply.github.com>
2023-09-18 18:32:59 +0200

    feat: logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,44 @@
 import { useEffect, useState } from 'react'
 import WährungBetragBlock from './WährungBetragBlock'
-import { v4 as uuidv4 } from "uuid"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRightArrowLeft } from '@fortawesome/free-solid-svg-icons'
 function App() {
 
   const [ausgangsCurrency, setAusgangsCurrency] = useState("EUR")
   const [andereCurrency, setAndereCurrency] = useState("USD")
   const [ausgangsCurrencyBetrag, setAusgangsCurrencyBetrag] = useState(0)
   const [andereCurrencyBetrag, setAndereCurrencyBetrag] = useState(0)
-
-  // fetch("https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_fah6bqBLJBnkdtiUUhZqtAU63TNdYu3dKb0fW8nB")
-  //   .then(res => res.json())
-  //   .then(data => console.log(data))
-
-  function umrechnung () {
-      // fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_fah6bqBLJBnkdtiUUhZqtAU63TNdYu3dKb0fW8nB&currencies=&base_currency=${ausgangsCurrency}`)
-      //  .then(res => res.json())
-      //  .then(data => console.log(data))
-  }
+  const [umrechnungsKurs, setUmrechnungskurs] = useState(0)
 
   function selectAusgangsCurrency(e) {
-    setAusgangsCurrency(e.target.value)
+    setAusgangsCurrency(e.value)
   }
 
   function selectZielCurrency(e) {
-   setAndereCurrency(e.target.value)
+   setAndereCurrency(e.value)
   }
+
+  useEffect(() => {
+      fetch(`https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_fah6bqBLJBnkdtiUUhZqtAU63TNdYu3dKb0fW8nB&currencies=&base_currency=${ausgangsCurrency}`)
+      .then(res => res.json())
+      .then(data => {
+        setUmrechnungskurs(data.data[andereCurrency])
+      })
+  },[ausgangsCurrency, andereCurrency])
+
+  useEffect(() => {
+    setAndereCurrencyBetrag(Math.round((ausgangsCurrencyBetrag * umrechnungsKurs)* 1000)/1000)
+  },[umrechnungsKurs, ausgangsCurrencyBetrag])
 
   function handleAusgangsBetrag(e) {
-    setAusgangsCurrencyBetrag(e.target.value)
+      setAusgangsCurrencyBetrag(e.target.value)
   }
-
-  function handleZielBetrag(e) {
-    setAndereCurrencyBetrag(e.target.value) 
-  }
-
+  
   return (
     <>
       <h1>Currency Converter</h1>  
-      <WährungBetragBlock change={selectAusgangsCurrency} handle={handleAusgangsBetrag} ausgangsCurrency={ausgangsCurrency}/>
-      <button><FontAwesomeIcon icon={faArrowRightArrowLeft} /></button>
-      <WährungBetragBlock change={selectZielCurrency} handle={handleZielBetrag}/>
-      <p>1,00 {ausgangsCurrency} = 1,36 {andereCurrency}</p>
+      <WährungBetragBlock change={selectAusgangsCurrency} handle={handleAusgangsBetrag} ausgang={ausgangsCurrency} value={ausgangsCurrencyBetrag}/>
+      <button>Pfeil</button>
+      <WährungBetragBlock change={selectZielCurrency} ausgang={andereCurrency} value={andereCurrencyBetrag}/>
+      <p>{Number(ausgangsCurrencyBetrag).toLocaleString("de-DE")} {ausgangsCurrency} = {andereCurrencyBetrag.toLocaleString("de-DE")} {andereCurrency}</p>
     </>
   )
 }
--- src/WährungBetragBlock.jsx
+++ src/WährungBetragBlock.jsx
@@ -1,25 +1,19 @@
-import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+// import { useState } from "react";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 export default function WährungBetragBlock(props) {
 
-    const [allCurrencys, setAllCurrencys] = useState([
+    const options = [
         "EUR", "USD", "JPY", "BGN", "CZK", "DKK", "GBP", "HUF", "PLN", "RON", "SEK", "CHF", "ISK","NOK", "HRK", "RUB", "TRY", 
         "AUD", "BRL", "CAD", "CNY", "HKD", "IDR", "ILS", "INR", "KRW", "MXN", "MYR", "NZD", "PHP", "SGD", "THB", "ZAR"
-    ])
+    ]
 
     return (
       <>
         <form>
-          <select onChange={props.change}>
-            {allCurrencys.map((item) => {
-              return <option 
-                key={uuidv4()}
-                id={item}
-                >{item}</option>;
-            })}
-          </select>
-          <input type="number" onChange={(e) => props.handle(e)}/>
+          <Dropdown className= "dropdown" options={options} onChange={props.change} value={props.ausgang} placeholder="Select an option" />
+          <input type="number" onChange={(e) => props.handle(e)} value={props.value}/>
         </form>
       </>
     );
